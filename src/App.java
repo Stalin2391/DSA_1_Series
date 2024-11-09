f7a2425ede12be97224bb8e54dec5d4ef03c6c44
Stalin2391 <stalind3@gmail.com>
2024-11-09 15:15:22 +0530

    reveresed array

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 import DsaArraysOne.Arrays;
 public class App {
     public static void main(String[] args) throws Exception {
-        Arrays.rotateArray();
+        int[] arr = {3,7,5,9,2,8};
+        Arrays.printArrayElements(arr);
+        System.out.println();
+        Arrays.reverseArray(arr);
     }
 }
--- src/DsaArraysOne/Arrays.java
+++ src/DsaArraysOne/Arrays.java
@@ -1,7 +1,27 @@
 package DsaArraysOne;
 
 public class Arrays {
-    public static void rotateArray(){
-        System.out.println("rotate testssss");
+    public static void printArrayElements(int[] arr){
+        int N = arr.length;
+        for(int i = 0; i < N; i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+    public static void reverseArray(int[] arr){
+        int N = arr.length;
+
+        int i = 0, j = N - 1;
+
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        
+        for(int k = 0; k < N; k++){
+            System.out.print(arr[k] + " "); 
+        }
     }
 }
